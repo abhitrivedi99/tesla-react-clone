d618523eec176c097fd3f4ecd70333af4913506f
abhiS99 <abhishek@solulab.co>
2021-06-12 11:04:41 +0530

    Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// import Header from './Header'
+import Home from './Home'
+
+const App = () => {
+	return (
+		<div className="App">
+			{/* <Header /> */}
+			<Home />
+			Hello
+		</div>
+	)
+}
+
+export default App
